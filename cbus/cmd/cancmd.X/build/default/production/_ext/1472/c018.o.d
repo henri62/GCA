26b066ff02f21468bd0c8e09d7345816d5b49b1f
rob <>
2011-08-03 18:20:58 +0200

    remove binaries from the pmlabs x setup

--- cbus/cmd/cancmd.X/build/default/production/_ext/1472/c018.o.d
+++
@@ -1,1 +0,0 @@
-build/default/production/_ext/1472/c018.o: ../c018.c
--- cbus/cmd/cancmd.X/build/default/production/_ext/1472/cancmd.o.d
+++
@@ -1,3 +0,0 @@
-build/default/production/_ext/1472/cancmd.o: ../cancmd.c ../project.h ../cancmd.h ../commands.h \
- ../power_cmds.h ../program_cmds.h ../isr_high.h ../mode_cmds.h \
- ../packet_gen.h ../cbus_common.h ../can_send.h ../cbusdefs7e.h
