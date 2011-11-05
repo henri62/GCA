6ccd00f87183b31bf390e53d506ef4304adc0cbc
rob <>
2011-11-05 13:54:24 +0100

    cangc4: trying to get 40kHz

--- cbus/cangc4/cangc4.c
+++ cbus/cangc4/cangc4.c
@@ -22,36 +22,56 @@
 
 #include "cangc4.h"
 
+__CONFIG(FOSC_INTRCIO & WDTE_OFF & PWRTE_ON & MCLRE_OFF & BOREN_OFF);
+
+
 void setup(void);
 
+static char f40khz;
 
 void main(void) {
-
+  f40khz = 0;
   setup();
   
   do {
-
+    f40khz ^= 1;
+    GPIO2 = f40khz;
   } while( True );
 }
 
 
 void setup(void) {
   // Timer0 Registers:
-  // Prescaler=1:1; TMR0 Preset=131; Freq=8.00kHz; Period=125,000 ns
-  /*
-  OPTION_REG.T0CS = 0;// bit 5 TMR0 Clock Source Select bit:0=Internal Clock (CLKO) / 1=Transition on T0CKI pin
-  OPTION_REG.T0SE = 0;// bit 4 TMR0 Source Edge Select bit: 0=low/high / 1=high/low
-  OPTION_REG.PSA  = 1;// bit 3 Prescaler Assignment bit: 0=Prescaler is assigned to the Timer0
-  OPTION_REG.PS2  = 0;// bits 2-0  PS2:PS0: Prescaler Rate Select bits
-  OPTION_REG.PS1  = 0;
-  OPTION_REG.PS0  = 0;
-  TMR0 = 131;           // preset for timer register
+  // Prescaler=1:1; TMR0 Preset=244; Freq=83.33333kHz; Period=12,000 ns
+  OPTION_REGbits.T0CS = 0;// bit 5 TMR0 Clock Source Select bit:0=Internal Clock (CLKO) / 1=Transition on T0CKI pin
+  OPTION_REGbits.T0SE = 0;// bit 4 TMR0 Source Edge Select bit: 0=low/high / 1=high/low
+  OPTION_REGbits.PSA  = 1;// bit 3 Prescaler Assignment bit: 0=Prescaler is assigned to the Timer0
+  OPTION_REGbits.PS2  = 0;// bits 2-0  PS2:PS0: Prescaler Rate Select bits
+  OPTION_REGbits.PS1  = 0;
+  OPTION_REGbits.PS0  = 0;
+  TMR0 = 244;           // preset for timer register
+
+  // Setup Timer0 interrupt.
+  INTCON = 0;
+  //INTCONbits.T0IE = 1;
+  INTCONbits.T0IF = 0;
+  //INTCONbits.GIE  = 1;
+
+  /* I/O
+  Port 	Use 	    Info bit
+  GP2 	IR-LED 1 	0
+  GP4 	IR-LED 2 	1
   */
-  
+  TRISIO = 0; // All output.
+  PIE1   = 0;
+  PIR1   = 0;
 }
 
 static void interrupt
 isr(void) // Here be interrupt function - the name is unimportant.
 {
-  INTF = 0; // clear the interrupt
+  TMR0 = 244; // preset for timer register
+  f40khz ^= 1;
+  GPIO2 = f40khz;
+  INTF = 0;   // clear the interrupt
 }
