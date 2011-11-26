626342446bac750a877c54a4f3ab7b083a4144f5
r.j.versluis@rocrail.net <>
2011-11-26 17:36:42 +0100

    gc1: set case sensitive

--- cbus/canrs.X/nbproject/Makefile-default.mk
+++ cbus/canrs.X/nbproject/Makefile-default.mk
@@ -11,8 +11,11 @@
 include Makefile
 
 # Environment
+# Adding MPLAB X bin directory to path
+PATH:=/Applications/microchip/mplabx/mplab_ide.app/Contents/Resources/mplab_ide/mplab_ide/modules/../../bin/:$(PATH)
 MKDIR=mkdir -p
 RM=rm -f 
+MV=mv 
 CP=cp 
 
 # Macros
@@ -31,6 +34,9 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 # Distribution Directory
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
+# Object Files Quoted if spaced
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/canrs.o
+
 # Object Files
 OBJECTFILES=${OBJECTDIR}/canrs.o
 
@@ -40,7 +46,7 @@ ASFLAGS=
 LDLIBSOPTIONS=
 
 # Path to java used to run MPLAB X when this makefile was created
-MP_JAVA_PATH=/usr/lib/jvm/java-6-openjdk/jre/bin/
+MP_JAVA_PATH="/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home/bin/"
 OS_CURRENT="$(shell uname -s)"
 ############# Tool locations ##########################################
 # If you copy a project from one host to another, the path where the  #
@@ -50,79 +56,53 @@ OS_CURRENT="$(shell uname -s)"
 #######################################################################
 # MP_CC is not defined
 # MP_BC is not defined
-MP_AS=/opt/microchip/mplabx/mpasmx/mpasmx
-MP_LD=/opt/microchip/mplabx/mpasmx/mplink
-MP_AR=/opt/microchip/mplabx/mpasmx/mplib
-# MP_BC is not defined
+MP_AS="/Applications/microchip/mplabx/mpasmx/mpasmx"
+MP_LD="/Applications/microchip/mplabx/mpasmx/mplink"
+MP_AR="/Applications/microchip/mplabx/mpasmx/mplib"
+DEP_GEN=${MP_JAVA_PATH}java -jar "/Applications/microchip/mplabx/mplab_ide.app/Contents/Resources/mplab_ide/mplab_ide/modules/../../bin/extractobjectdependencies.jar" 
+# fixDeps replaces a bunch of sed/cat/printf statements that slow down the build
+FIXDEPS=fixDeps
 # MP_CC_DIR is not defined
 # MP_BC_DIR is not defined
-MP_AS_DIR=/opt/microchip/mplabx/mpasmx
-MP_LD_DIR=/opt/microchip/mplabx/mpasmx
-MP_AR_DIR=/opt/microchip/mplabx/mpasmx
+MP_AS_DIR="/Applications/microchip/mplabx/mpasmx"
+MP_LD_DIR="/Applications/microchip/mplabx/mpasmx"
+MP_AR_DIR="/Applications/microchip/mplabx/mpasmx"
 # MP_BC_DIR is not defined
 
-.build-conf: ${BUILD_SUBPROJECTS}
+.build-conf:  ${BUILD_SUBPROJECTS}
 	${MAKE}  -f nbproject/Makefile-default.mk dist/${CND_CONF}/${IMAGE_TYPE}/canrs.X.${IMAGE_TYPE}.cof
 
 MP_PROCESSOR_OPTION=18f2480
 MP_LINKER_DEBUG_OPTION= -u_DEBUGCODESTART=0x3dc0 -u_DEBUGCODELEN=0x240 -u_DEBUGDATASTART=0x2f4 -u_DEBUGDATALEN=0xb
 # ------------------------------------------------------------------------------------
-# Rules for buildStep: createRevGrep
-ifeq ($(TYPE_IMAGE), DEBUG_RUN)
-__revgrep__:   nbproject/Makefile-${CND_CONF}.mk
-	@echo 'grep -q $$@' > __revgrep__
-	@echo 'if [ "$$?" -ne "0" ]; then' >> __revgrep__
-	@echo '  exit 0' >> __revgrep__
-	@echo 'else' >> __revgrep__
-	@echo '  exit 1' >> __revgrep__
-	@echo 'fi' >> __revgrep__
-	@chmod +x __revgrep__
-else
-__revgrep__:   nbproject/Makefile-${CND_CONF}.mk
-	@echo 'grep -q $$@' > __revgrep__
-	@echo 'if [ "$$?" -ne "0" ]; then' >> __revgrep__
-	@echo '  exit 0' >> __revgrep__
-	@echo 'else' >> __revgrep__
-	@echo '  exit 1' >> __revgrep__
-	@echo 'fi' >> __revgrep__
-	@chmod +x __revgrep__
-endif
-
-# ------------------------------------------------------------------------------------
 # Rules for buildStep: assemble
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
-.PHONY: ${OBJECTDIR}/canrs.o
-${OBJECTDIR}/canrs.o: canrs.asm __revgrep__ nbproject/Makefile-${CND_CONF}.mk
-	${MKDIR} ${OBJECTDIR} 
-ifneq (,$(findstring MINGW32,$(OS_CURRENT))) 
-	-${MP_AS} $(MP_EXTRA_AS_PRE) -d__DEBUG -d__MPLAB_DEBUGGER_PK3=1 -q -p$(MP_PROCESSOR_OPTION)  -l"${OBJECTDIR}/canrs.lst" -e"${OBJECTDIR}/canrs.err" $(ASM_OPTIONS) -o"${OBJECTDIR}/canrs.o" canrs.asm 
-else 
-	-${MP_AS} $(MP_EXTRA_AS_PRE) -d__DEBUG -d__MPLAB_DEBUGGER_PK3=1 -q -p$(MP_PROCESSOR_OPTION) -u  -l"${OBJECTDIR}/canrs.lst" -e"${OBJECTDIR}/canrs.err" $(ASM_OPTIONS) -o"${OBJECTDIR}/canrs.o" canrs.asm 
-endif 
-	@cat  "${OBJECTDIR}/canrs.err" | sed -e 's/\x0D$$//' -e 's/\(^Warning\|^Error\|^Message\)\(\[[0-9]*\]\) *\(.*\) \([0-9]*\) : \(.*$$\)/\3:\4: \1\2: \5/g'
-	@./__revgrep__ "^Error" ${OBJECTDIR}/canrs.err
+${OBJECTDIR}/canrs.o: canrs.asm  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} ${OBJECTDIR} 
+	@${RM} ${OBJECTDIR}/canrs.o.d 
+	@${FIXDEPS} dummy.d -e "${OBJECTDIR}/canrs.err" $(SILENT) -c ${MP_AS} $(MP_EXTRA_AS_PRE) -d__DEBUG -d__MPLAB_DEBUGGER_PK3=1 -q -p$(MP_PROCESSOR_OPTION) -u  -l\"${OBJECTDIR}/canrs.lst\" -e\"${OBJECTDIR}/canrs.err\" $(ASM_OPTIONS) -o\"${OBJECTDIR}/canrs.o\" canrs.asm 
+	
+	@${DEP_GEN} -d ${OBJECTDIR}/canrs.o 
+	
 else
-.PHONY: ${OBJECTDIR}/canrs.o
-${OBJECTDIR}/canrs.o: canrs.asm __revgrep__ nbproject/Makefile-${CND_CONF}.mk
-	${MKDIR} ${OBJECTDIR} 
-ifneq (,$(findstring MINGW32,$(OS_CURRENT))) 
-	-${MP_AS} $(MP_EXTRA_AS_PRE) -q -p$(MP_PROCESSOR_OPTION)  -l"${OBJECTDIR}/canrs.lst" -e"${OBJECTDIR}/canrs.err" $(ASM_OPTIONS) -o"${OBJECTDIR}/canrs.o" canrs.asm 
-else 
-	-${MP_AS} $(MP_EXTRA_AS_PRE) -q -p$(MP_PROCESSOR_OPTION) -u  -l"${OBJECTDIR}/canrs.lst" -e"${OBJECTDIR}/canrs.err" $(ASM_OPTIONS) -o"${OBJECTDIR}/canrs.o" canrs.asm 
-endif 
-	@cat  "${OBJECTDIR}/canrs.err" | sed -e 's/\x0D$$//' -e 's/\(^Warning\|^Error\|^Message\)\(\[[0-9]*\]\) *\(.*\) \([0-9]*\) : \(.*$$\)/\3:\4: \1\2: \5/g'
-	@./__revgrep__ "^Error" ${OBJECTDIR}/canrs.err
+${OBJECTDIR}/canrs.o: canrs.asm  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} ${OBJECTDIR} 
+	@${RM} ${OBJECTDIR}/canrs.o.d 
+	@${FIXDEPS} dummy.d -e "${OBJECTDIR}/canrs.err" $(SILENT) -c ${MP_AS} $(MP_EXTRA_AS_PRE) -q -p$(MP_PROCESSOR_OPTION) -u  -l\"${OBJECTDIR}/canrs.lst\" -e\"${OBJECTDIR}/canrs.err\" $(ASM_OPTIONS) -o\"${OBJECTDIR}/canrs.o\" canrs.asm 
+	
+	@${DEP_GEN} -d ${OBJECTDIR}/canrs.o 
+	
 endif
 
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: link
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
 dist/${CND_CONF}/${IMAGE_TYPE}/canrs.X.${IMAGE_TYPE}.cof: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk
-	${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
+	@${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
 	${MP_LD} $(MP_EXTRA_LD_PRE)   -p$(MP_PROCESSOR_OPTION)  -w -x -u_DEBUG -z__ICD2RAM=1  -z__MPLAB_BUILD=1  -z__MPLAB_DEBUG=1 -z__MPLAB_DEBUGGER_PK3=1 $(MP_LINKER_DEBUG_OPTION) -odist/${CND_CONF}/${IMAGE_TYPE}/canrs.X.${IMAGE_TYPE}.cof ${OBJECTFILES}     
 else
 dist/${CND_CONF}/${IMAGE_TYPE}/canrs.X.${IMAGE_TYPE}.cof: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk
-	${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
+	@${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
 	${MP_LD} $(MP_EXTRA_LD_PRE)   -p$(MP_PROCESSOR_OPTION)  -w   -z__MPLAB_BUILD=1  -odist/${CND_CONF}/${IMAGE_TYPE}/canrs.X.${IMAGE_TYPE}.cof ${OBJECTFILES}     
 endif
 
@@ -138,4 +118,7 @@ endif
 # Enable dependency checking
 .dep.inc: .depcheck-impl
 
-include .dep.inc
+DEPFILES=$(wildcard $(addsuffix .d, ${OBJECTFILES}))
+ifneq (${DEPFILES},)
+include ${DEPFILES}
+endif
